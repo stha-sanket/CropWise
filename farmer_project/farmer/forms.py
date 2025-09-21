1241152e6d246aa85411222374f9a0c319ee9434
stha-sanket <sanketshrestha09@gmail.com>
2025-09-21 19:22:16 +0545

    final ui design for signup and login

--- farmer_project/farmer/forms.py
+++ farmer_project/farmer/forms.py
@@ -7,28 +7,19 @@ class CustomUserCreationForm(UserCreationForm):
         widgets = {
             'username': forms.TextInput(attrs={
                 'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
-                'placeholder': 'Enter your username'
             }),
             'email': forms.EmailInput(attrs={
                 'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
-                'placeholder': 'Enter your email'
             }),
-            'password1': forms.PasswordInput(attrs={
+            'password': forms.PasswordInput(attrs={
                 'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
-                'placeholder': 'Enter your password'
-            }),
-            'password2': forms.PasswordInput(attrs={
-                'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
-                'placeholder': 'Confirm your password'
             }),
         }
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
-        'placeholder': 'Enter your username'
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
-        'placeholder': 'Enter your password'
     }))
