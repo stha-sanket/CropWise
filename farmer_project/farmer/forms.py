075bcffabb67046c244c265373660310c38641f2
stha-sanket <sanketshrestha09@gmail.com>
2025-09-21 18:57:56 +0545

    new look

--- farmer_project/farmer/forms.py
+++ farmer_project/farmer/forms.py
@@ -5,10 +5,30 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:border-green-500 transition', 'placeholder': 'Enter your username'}),
-            'email': forms.EmailInput(attrs={'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:border-green-500 transition', 'placeholder': 'Enter your email'}),
+            'username': forms.TextInput(attrs={
+                'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+                'placeholder': 'Enter your username'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+                'placeholder': 'Enter your email'
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+                'placeholder': 'Enter your password'
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+                'placeholder': 'Confirm your password'
+            }),
         }
 
 class CustomAuthenticationForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:border-green-500 transition', 'placeholder': 'Enter your username'}))
-    # Password will be rendered manually in the template
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+        'placeholder': 'Enter your username'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+        'placeholder': 'Enter your password'
+    }))
