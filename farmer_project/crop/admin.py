26f83624406f941b3af3feb28ef07aac96eaee9c
stha-sanket <sanketshrestha09@gmail.com>
2025-09-21 19:45:17 +0545

    removed crop management system

--- farmer_project/crop/admin.py
+++ farmer_project/crop/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import LearningContent, Crop
+from .models import LearningContent
 
 # Register your models here.
 admin.site.register(LearningContent)
-admin.site.register(Crop)
