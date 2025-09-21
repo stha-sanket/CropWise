a8953151ad77a5abacca440cbdb35ca30449305b
stha-sanket <sanketshrestha09@gmail.com>
2025-09-21 19:25:23 +0545

    fixed the signup bug

--- farmer_project/farmer/forms.py
+++ farmer_project/farmer/forms.py
@@ -6,20 +6,20 @@ class CustomUserCreationForm(UserCreationForm):
         fields = UserCreationForm.Meta.fields + ('email',)
         widgets = {
             'username': forms.TextInput(attrs={
-                'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+                'class': 'input-focus w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
             }),
             'email': forms.EmailInput(attrs={
-                'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+                'class': 'input-focus w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
             }),
             'password': forms.PasswordInput(attrs={
-                'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+                'class': 'input-focus w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
             }),
         }
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+        'class': 'input-focus w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'input-focus pl-10 w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
+        'class': 'input-focus w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none transition placeholder-gray-500',
     }))
